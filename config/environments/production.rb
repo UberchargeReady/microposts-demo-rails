4d93ca5f1020aaa800b4afab17a10871367d1d05
George <ubercharge.ready@gmail.com>
2016-09-05 16:43:07 +0300

    try moving carrierwave config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,17 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV['S3_REGION']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
